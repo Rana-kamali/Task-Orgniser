cc1d22e33297140c5ff388aca1ff3d96d2301947
Rana-Kamali <raana.kamalinezhad@gmail.com>
2021-02-24 21:11:59 +1100

    editing backend codes

---
+++ src/Component/Login/loginApp.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import LoginForm from "./LoginForm"
+import {Welcome} from "../NewList/Welcome"
+import{TaskForm} from "../NewList/TaskForm"
+
+function LoginApp() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  };
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+
+    if(details.email === adminUser.email && details.password === adminUser.password){
+        console.log("you have logged in")
+        setUser({
+            name: details.name,
+            email: details.email,
+        })
+    }else {
+        console.log("Details not match")
+        setError("Details not match")
+    }
+  };
+
+  const logout = () => {
+    console.log("loggedOut");
+    setUser ({name: "", email: ""  })
+  };
+
+  return <div className="Login">
+{(user.email !=="") ? (
+<div className = "welcome">
+    <h2> Welcome, <span>{user.name}</span></h2>
+<button onClick={logout}>Logout</button>
+<Welcome/>
+<TaskForm/>
+</div>
+
+):(
+    <LoginForm Login={Login} error= {error}/>
+)}
+
+
+  </div>;
+}
+
+export default LoginApp;
---
+++ src/Component/NewList/Welcome.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Welcome = () => {
+  return (
+    <div>
+      <p>Add A Task </p>
+      <p> Check Your Task</p>
+    </div>
+  );
+};
+export { Welcome };
